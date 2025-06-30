4d7dee6965ad3d24c02efcab2f9343016a4b68d9
JS-Wagner <aguswagnercalvo@hotmail.com>
2025-06-29 21:33:54 -0300

    Build para GitHub Pages y corrige base href

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HKKYBMTP.mjs";import"./chunk-DI3FLPCK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-CAYYHKZU.mjs";import"./chunk-DI3FLPCK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
